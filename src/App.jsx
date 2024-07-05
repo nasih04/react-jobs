4c6b6ca3da741e057d66c666ee40626f3cc15143
nasih04 <dev.nasih@gmail.com>
2024-07-05 20:29:52 +0530

    conditional rendedering and array rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-
 const App = () => {
+  const name = "john";
+  const x = 10;
+  const y  = 20;
+  const names = ["john", "jane", "joe"];
+  const loggedIn = false;
+
   return (
    <>
-   <h1 className="text-red-300 text-4xl">Nasih is the boss here</h1>
+    <h1>hello {name}</h1>
+    <p>The sum of {x} and {y} is {x+y}</p>
+    <ul>
+      {names.map((name, index) => (
+        <li key={index}>{name}</li>
+      ))}
+    </ul>
+    {loggedIn ? <p>Hello Member</p> : <p>Hello Guest</p>}
    </>
   )
 }
